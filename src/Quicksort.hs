c07b946a3b0a99a0199d407171d711057c14bd9b
Chris Done <chrisdone@gmail.com>
2014-12-24 23:54:08 +0100

    Add in-place quicksort

--- src/Quicksort.hs
+++ src/Quicksort.hs
@@ -1,3 +1,89 @@
--- | Sorting algorithms
+-- | In-place quicksort.
 
-module Quicksort where
+module QuickSort (quickSort) where
+
+import           Control.Monad
+import           Control.Monad.Primitive
+import           Control.Monad.ST
+import           Data.List (sort)
+import           Data.Vector (Vector)
+import qualified Data.Vector as V (toList)
+import qualified Data.Vector.Generic as V(freeze)
+import           Data.Vector.Mutable (MVector)
+import qualified Data.Vector.Mutable as V
+import           Prelude hiding (read)
+import           Test.QuickCheck
+
+--------------------------------------------------------------------------------
+-- Implementation
+
+-- | In-place quicksort the given vector in some mutation-permitting
+-- monad.
+quickSort :: (PrimMonad m,Ord a) => MVector (PrimState m) a -> m ()
+quickSort vec =
+  qsort vec 0 (V.length vec)
+  where qsort array begin end =
+          when (end > begin)
+               (do let startP = begin + ((end - begin) `div` 2)
+                   pivot <- partition array begin end startP
+                   if pivot - begin > end - pivot + 1
+                      then do qsort array (pivot + 1) end
+                              qsort array begin pivot
+                      else do qsort array begin pivot
+                              qsort array (pivot + 1) end)
+
+-- | Swap elements in the array until all elements <pivot are to the
+-- left of pivot.
+partition :: (PrimMonad m,Ord a) => V.MVector (PrimState m) a -> Int -> Int -> Int -> m Int
+partition array begin end pivot =
+  do piv <- V.read array pivot
+     V.swap array pivot (end - 1)
+     store <- foldM (\store ix ->
+                       do v <- V.read array ix
+                          if v <= piv
+                             then do V.swap array store ix
+                                     return (store + 1)
+                             else return store)
+                    begin
+                    [begin .. end - 2]
+     V.swap array (end - 1) store
+     return store
+
+--------------------------------------------------------------------------------
+-- Tests
+
+-- | Test that sorting some list of ints is equivalent to @sort xs@.
+quickSortProp :: [Int] -> Bool
+quickSortProp xs =
+  sort xs ==
+  V.toList (runST (do arr <- thaw xs
+                      quickSort arr
+                      freeze arr))
+
+--------------------------------------------------------------------------------
+-- Example
+
+main :: IO ()
+main =
+  do quickCheck quickSortProp
+     ioVector <- do arr <- thaw [1,7,2,4,1,8,5,2]
+                    quickSort arr
+                    freeze arr
+     print ioVector
+     print (runST (do arr <- thaw [1,7,2,4,1,8,5,2]
+                      quickSort arr
+                      freeze arr))
+
+-- | Handy function to construct a mutable vector from a list.
+thaw :: (PrimMonad m)
+     => [Int] -> m (MVector (PrimState m) Int)
+thaw is =
+  do array <- V.new (length is)
+     forM_ (zip [0 ..] is)
+           (\(i,v) -> V.write array i (v :: Int))
+     return array
+
+-- | More specific type for freezing.
+freeze :: (PrimMonad m)
+       => MVector (PrimState m) a -> m (Vector a)
+freeze = V.freeze
